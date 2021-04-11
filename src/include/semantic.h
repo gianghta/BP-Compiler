7dabf9d5e47f2983d557a9b55a129234ed8370e0
Giang Ta <thoanggiang98@gmail.com>
2021-04-10 22:58:03 -0400

    init semantic analysis

--- src/include/semantic.h
+++ src/include/semantic.h
@@ -1,12 +1,25 @@
 #ifndef SEMANTIC_H
 #define SEMANTIC_H
 
+#include <stdbool.h> 
+
 #include "scope.h"
 
-typedef struct SemanticAnalyzer {
+typedef struct Semantic {
     Scope* global;
     Scope* current_local;
-} SemanticAnalyzer;
+} Semantic;
 
+Semantic* init_semantic_analyzer();
+void create_new_scope(Semantic* sem);
+void exit_current_scope(Semantic* sem);
+void set_symbol(Semantic* sem, char* s, Symbol sym, bool is_global);
+Symbol get_current_symbol(Semantic* sem, char* s);
+Symbol get_current_global_symbol(Semantic* sem, char* s, bool is_global);
+void has_current_symbol(Semantic* sem, char* s);
+void has_current_global_symbol(Semantic* sem, char* s, bool is_global);
 
-#endif SEMANTIC_H
+void set_current_procedure(Semantic* sem, Symbol proc);
+Symbol get_current_procedure(Semantic* sem);
+
+#endif
--- src/semantic.c
+++ src/semantic.c
@@ -1,1 +1,9 @@
 #include "include/semantic.h"
+
+
+Semantic* init_semantic_analyzer()
+{
+    Semantic* sem = calloc(1, sizeof(struct Semantic));
+    sem->global = init_scope();
+    sem->current_local = sem->global;
+}
